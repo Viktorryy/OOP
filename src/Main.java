d98d93f4103f7d862bb4dcdb59831af4bc8aa821
Viktorryy <viktory_o@rambler.ru>
2022-10-20 10:12:05 +0400

    Добрый день!! Третий день не могу разобраться почему не правильно выводится печать!! Пробовала разными способами все равно выводится только последняя запись. Почему так происходит??

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  ");
+
+        // Задание 1
+        Human[] humans = new Human[4];
+        humans[0] = new Human("Максим",1987,"Минск", "бренд-менеджером");
+        humans[1] = new Human("Аня",1993,"Москва","методистом образовательных программ");
+        humans[2] = new Human("Катя", 1994, "Калининград","продакт-менеджером");
+        humans[3] = new Human("Артем", 1995, "Москва","директором по развитию бизнеса");
+
+        for (int i = 0; i < humans.length; i++) {
+            System.out.println( "    i  " + i);
+            System.out.println("Привет! Меня зовут " +humans[i].getName() + ". Я из города " + humans[i].getTown() +
+                    ". Я родился в " + humans[i].getYearOfBirth() + " году. Будем знакомы!!");
+
+           Human.printEmployee1(humans[i]);
+        }
+
+        // Задание 2
+
+
+        for (int i = 0; i < humans.length; i++) {
+
+            Human.printEmployee2(humans[i]);
+
+        }
+
+
     }
 
 }
